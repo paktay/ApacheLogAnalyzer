9c4e9b5f343a33624c318ddbea44972f03631200
Ricky <ricky@rti.co.id>
2023-10-20 11:29:56 +0700

    add check filter text

--- src/apacheloganalyzer/ApacheLogAnalyzer.java
+++ src/apacheloganalyzer/ApacheLogAnalyzer.java
@@ -14,20 +14,23 @@ public class ApacheLogAnalyzer {
 
     public static void main(String[] args) throws Exception {
         // Read the Apache web server log file        
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        File file = new File(args[0]);
+        String fileName = file.getName();
+        
+        BufferedReader reader = new BufferedReader(new FileReader(file));
 
         // Create a map to store the summary data
         //Map<String, Map<String, Summary>> summary = new HashMap<>();
-        Map<String, Map<String, Object>> summary = new HashMap<>();
+        Map<String, Map<String, Summary>> summary = new HashMap<>();
         Map<String, Map<String, Summary>> summary2 = new HashMap<>();
         
         // Create a map to store the log data per IP
-        Map<String, List<LogEntry>> logDataPerIp = new HashMap<>();
+        //Map<String, List<LogEntry>> logDataPerIp = new HashMap<>();
         
         // Iterate over the log file and update the summary map
         String line;
-        int lineNumber = 1;
-        int jsonFileNumber = 1;
+//        int lineNumber = 1;
+//        int jsonFileNumber = 1;
         while ((line = reader.readLine()) != null) {
             // Split the log line into fields
             String[] fields = line.split(" ");
@@ -38,36 +41,23 @@ public class ApacheLogAnalyzer {
             String timestamp = fields[3]+ " " + fields[4];
             timestamp.replace("[", "").replace("]", "");
             
-            if(FilterUrl.validate(url)) {
+            if(FilterUrl.validate(url, args)) {
                 url = FilterUrl.stripUrl(url);
                 
                 int statusCode = Integer.parseInt(fields[8]);
 
                 // If the HTTP status code is 200, update the hit counter for the IP and URL
                 if (statusCode == 200) {
-                    Map<String, Object> urlSummary = summary.getOrDefault(ip, new HashMap<>());
+                    //Map<String, Summary> urlSummary = summary.getOrDefault(ip, new HashMap<>());
                     //List<Map<String, Integer>> urlList = (List) urlSummary.get("urls");
                     //List<Map<String, Integer>> urlList = (List) urlSummary.getOrDefault("urls", new ArrayList<>());
-                    Map<String, Integer> urlList = (Map<String, Integer>) urlSummary.getOrDefault("urls", new HashMap<>());
-                    urlList.put(url, urlList.getOrDefault(url, 0) + 1);
+                    //Map<String, Integer> urlList = (Map<String, Integer>) urlSummary.getOrDefault("urls", new HashMap<>());
+                    //urlList.put(url, urlList.getOrDefault(url, 0) + 1);
                     //urlList = checkIfKeyExists(urlList, url);
                     
-                    /*
-                    if(hitCount == 0) {
-                        Map<String, Integer> urlObj = new HashMap<>();
-                        urlObj.put(url, ++hitCount);
-                        urlList.add(urlObj);
-                    } else {
-                        urlList.get
-                    }
-                    */
-                    //Integer hitCount = (Integer) urlSummary.getOrDefault(url, (Object) 0);
-                    Integer total_hit = (Integer) urlSummary.getOrDefault("total_hit", (Object) 0);                    
-                    urlSummary.put("urls", urlList);
-                    urlSummary.put("total_hit", ++total_hit);
-                    summary.put(ip, urlSummary);
                     
-                    /*
+                    
+                    
                     Map<String, Summary> urlSummary = summary.getOrDefault(ip, new HashMap<>());                
                     Summary summaryObject = urlSummary.getOrDefault(url, new Summary());
                     summaryObject.hitCount++;
@@ -83,83 +73,86 @@ public class ApacheLogAnalyzer {
                     summary2.put(url, ipSummary);
                     
                     // Create a LogEntry object
-                    LogEntry logEntry = new LogEntry(ip, timestamp, url);
+                    //LogEntry logEntry = new LogEntry(ip, timestamp, url);
 
                     // Add the LogEntry object to the list of LogEntry objects for the IP address                    
-                    List<LogEntry> arr = new ArrayList<LogEntry>();
-                    arr.add(logEntry);
-                    logDataPerIp.computeIfAbsent(ip, k -> new ArrayList<LogEntry>()).add(logEntry);                   
-                    */
+                    //List<LogEntry> arr = new ArrayList<LogEntry>();
+                    //arr.add(logEntry);
+                    //logDataPerIp.computeIfAbsent(ip, k -> new ArrayList<LogEntry>()).add(logEntry);                   
                     
-                    if (lineNumber++ >= 10000) {
-                        lineNumber = 1;
-                        
-                        writeToJsonFile(summary, jsonFileNumber);
-                        jsonFileNumber++;
-                        summary = new HashMap<>();
-                    }
+                    
+//                    if (lineNumber++ >= 10000) {
+//                        lineNumber = 1;
+//                        
+//                        writeToJsonFile(summary, jsonFileNumber);
+//                        jsonFileNumber++;
+//                        summary = new HashMap<>();
+//                    }
                 }
             }
         }
         
-        if(lineNumber >= 1) {
-            writeToJsonFile(summary, jsonFileNumber);
-            //jsonFileNumber++;
-            //summary = new HashMap<>();
-        }
+//        if(lineNumber >= 1) {
+//            writeToJsonFile(summary, jsonFileNumber);
+//            //jsonFileNumber++;
+//            //summary = new HashMap<>();
+//        }
 
         // Close the reader
         reader.close();
 
         // Sort the summary map by total hit counter descending
-        /*
+        
         List<Map.Entry<String, Map<String, Summary>>> sortedSummary = new ArrayList<>(summary.entrySet());
         sortedSummary.sort((o1, o2) -> Integer.compare(o2.getValue().values().stream().mapToInt(summaryObject -> summaryObject.totalHit).sum(), o1.getValue().values().stream().mapToInt(summaryObject -> summaryObject.totalHit).sum()));
 
         List<Map.Entry<String, Map<String, Summary>>> sortedSummary2 = new ArrayList<>(summary2.entrySet());
         sortedSummary2.sort((o1, o2) -> Integer.compare(o2.getValue().values().stream().mapToInt(summaryObject2 -> summaryObject2.totalHit).sum(), o1.getValue().values().stream().mapToInt(summaryObject2 -> summaryObject2.totalHit).sum()));
 
-        writeToFile(sortedSummary, summary, "ip", "urls", args[0] + "1.json");
-        writeToFile(sortedSummary2, summary2, "url", "ips", args[0] + "2.json");
+        writeToFile(sortedSummary, summary, "ip", "urls", fileName + "_ip.json");        
+        summary.clear();
+        
+        writeToFile(sortedSummary2, summary2, "url", "ips", fileName + "_url.json");
+        summary2.clear();
               
         // Create a directory to store the split log files
-        File splitLogDirectory = new File(args[0].replace(".log", ""));
-        if (!splitLogDirectory.exists() || !splitLogDirectory.isDirectory()) {
-            System.out.println("directory not exist");
-            if(splitLogDirectory.mkdir()) {
-                System.out.println("create dir");
-            } else {
-                System.out.println("cannot create dir");
-            }
-        }
+//        File splitLogDirectory = new File(args[0].replace(".log", ""));
+//        if (!splitLogDirectory.exists() || !splitLogDirectory.isDirectory()) {
+//            System.out.println("directory not exist");
+//            if(splitLogDirectory.mkdir()) {
+//                System.out.println("create dir");
+//            } else {
+//                System.out.println("cannot create dir");
+//            }
+//        }
         
         // Write the log data per IP to JSON files
-        for (Map.Entry<String, List<LogEntry>> entry : logDataPerIp.entrySet()) {
-            String ipAddress = entry.getKey();
-            List<LogEntry> logEntries = entry.getValue();
+//        for (Map.Entry<String, List<LogEntry>> entry : logDataPerIp.entrySet()) {
+//            String ipAddress = entry.getKey();
+//            List<LogEntry> logEntries = entry.getValue();
+//
+//            // Create a JSON file for the IP address
+//            File logFile = new File(splitLogDirectory, ipAddress + ".json");
+//
+//            // Create a JSON array to store the LogEntry objects
+//            List<Map<String, Object>> jsonArray = new ArrayList<>();
+//            for (LogEntry logEntry : logEntries) {
+//                Map<String, Object> jsonObject = new HashMap<>();
+//                jsonObject.put("ip", logEntry.getIpAddress());
+//                jsonObject.put("timestamp", logEntry.getTimestamp());
+//                jsonObject.put("url", logEntry.getUrl());
+//
+//                jsonArray.add(jsonObject);
+//            }
+//
+//            // Write the JSON array to the JSON file
+//            FileWriter writer = new FileWriter(logFile);
+//            Gson gson = new Gson();
+//            writer.write(gson.toJson(jsonArray));
+//            writer.close();
+//
+//        }
 
-            // Create a JSON file for the IP address
-            File logFile = new File(splitLogDirectory, ipAddress + ".json");
-
-            // Create a JSON array to store the LogEntry objects
-            List<Map<String, Object>> jsonArray = new ArrayList<>();
-            for (LogEntry logEntry : logEntries) {
-                Map<String, Object> jsonObject = new HashMap<>();
-                jsonObject.put("ip", logEntry.getIpAddress());
-                jsonObject.put("timestamp", logEntry.getTimestamp());
-                jsonObject.put("url", logEntry.getUrl());
-
-                jsonArray.add(jsonObject);
-            }
-
-            // Write the JSON array to the JSON file
-            FileWriter writer = new FileWriter(logFile);
-            Gson gson = new Gson();
-            writer.write(gson.toJson(jsonArray));
-            writer.close();
-
-        }
-*/
     }
     
     private static void writeToJsonFile(Map<String, Map<String, Object>> _summary, int _jsonFileNumber) {
@@ -185,6 +178,16 @@ public class ApacheLogAnalyzer {
             String _tag,
             String _subtag,
             String _fileName) throws Exception {
+        
+        File directory = new File("data");
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.out.println("directory doesn't exist");
+            if(directory.mkdir()) {
+                System.out.println("create dir");
+            } else {
+                System.out.println("cannot create dir");
+            }
+        }
         
         // Convert the sorted summary map to a JSON array
         Gson gson = new Gson();
@@ -212,7 +215,7 @@ public class ApacheLogAnalyzer {
         }
 
         // Write the JSON array to a file
-        FileWriter writer = new FileWriter(_fileName);
+        FileWriter writer = new FileWriter("data/"+_fileName);
         writer.write(gson.toJson(jsonArray));
         writer.close();
     }
@@ -283,29 +286,57 @@ public class ApacheLogAnalyzer {
 
     
     private static class FilterUrl {
-        static List<String> urls = Arrays.asList(
-                "business.calendar_",
-                "business.e_ipo_",
-                "business.market_",
-                "business.indices_",
-                "business.forex_",
-                "business.metal_",
-                "business.movers_",
-                "business.news_",
-                "business.news_story_",
-                "business.special_notation_",
-                "business.stock_analytics_",
-                "business.stock_corpaction_",
-                "business.stock_financial_",
-                "business.stock_keystats_",
-                "business.stock_last_",
-                "business.stock_news_",
-                "business.stock_overview_",
-                "business.stock_profile_",
-                "business.watchlist_",
-                "stk_30days_diary.jsp",
-                "idx_30days_diary.jsp"
-        );
+//        static List<String> urls = Arrays.asList(
+//                "business.calendar_",
+//                "business.e_ipo_",
+//                "business.market_",
+//                "business.indices_",
+//                "business.forex_",
+//                "business.metal_",
+//                "business.movers_",
+//                "business.news_",
+//                "business.news_story_",
+//                "business.special_notation_",
+//                "business.stock_analytics_",
+//                "business.stock_corpaction_",
+//                "business.stock_financial_",
+//                "business.stock_keystats_",
+//                "business.stock_last_",
+//                "business.stock_news_",
+//                "business.stock_overview_",
+//                "business.stock_profile_",
+//                "business.watchlist_",
+//                "stk_30days_diary.jsp",
+//                "idx_30days_diary.jsp"
+//        );
+        static ArrayList<String> urls = null;
+        
+        
+        public static ArrayList<String> ReadFileLineByLine(String _file) 
+            throws FileNotFoundException, IOException {
+            // Create an ArrayList to store the lines of the file.
+            ArrayList<String> lines = new ArrayList<>();
+            File file = new File(_file);
+
+            // Create a BufferedReader to read the file line by line.
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            // Read each line of the file and add it to the ArrayList.
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Close the BufferedReader.
+            br.close();
+
+            // Print the contents of the ArrayList.
+//            for (String element : lines) {
+//                System.out.println(element);
+//            }
+            
+            return lines;
+        }
         
         public static String stripUrl(String url) {
             String result = url;
@@ -319,13 +350,26 @@ public class ApacheLogAnalyzer {
             return result;
         }
         
-        public static boolean validate(String url) {
+        public static boolean validate(String url, String[] args) {
             boolean result = false;
-            for (String element : urls){
-                if(url.contains(element)){
-                    result = true;
-                    break;
-                }            
+            if(args != null && args.length == 2) {
+                //ArrayList<String> urls = new ArrayList<>();
+                try {
+                    if(urls == null) {
+                        urls = ReadFileLineByLine(args[1]);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+  
+                for (String element : urls){
+                    if(url.contains(element)){
+                        result = true;
+                        break;
+                    }            
+                }
+            } else {
+                result = true;
             }
             return result;
         }
